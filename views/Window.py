9599189e7e4caadca03445695aad1c3a8cb8fb4f
Sevak Mardirosian <smardirosians@gmail.com>
2016-02-02 19:48:54 +0100

    small things

---
+++ views/Window.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Window(object):
+    """docstring for view"""
+
+    def __init__(self, arg):
+        super(Window, self).__init__()
+        self.arg = arg

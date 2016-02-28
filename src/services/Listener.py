93bf581fde154d110d5cdd941894f5a8af446749
Sevak .M <s1079770@student.hsleiden.nl>
2016-02-28 18:27:32 +0100

    changes

--- src/services/Listener.py
+++ src/services/Listener.py
@@ -2,12 +2,14 @@ import json
 import os
 import re
 import time
-
+import sys
 import regex as regex
 import tweepy
 from src.models import Twitter
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tweepy import StreamListener
+
 
 """ Read the config file"""
 f = open("../config/config.json")
@@ -17,27 +19,55 @@ f.close()
 
 
 # This is a basic listener that just prints received tweets to stdout.
-class Listener(tweepy.StreamListener):
-    def __init__(self):
-        super(Listener, self).__init__()
-      #  self.identifier = keyword
+class Listener(StreamListener):
+
+    def __init__(self, api = None, fprefix = 'streamer'):
+
+        self.api = api or API()
+        self.counter = 0
+        self.fprefix = fprefix
+        self.output  = open(fprefix + '.'
+                            + time.strftime('%Y%m%d-%H%M%S') + '.json', 'w')
+        self.delout  = open('delete.txt', 'a')
+
         self.time = int(time.time())
         self.tweets_data_path = "../data/tweets.json"
 
     def on_data(self, data):
-        print(data)
-        return True
+
+        if  'in_reply_to_status' in data:
+            self.on_status(data)
+        elif 'delete' in data:
+            delete = json.loads(data)['delete']['status']
+            if self.on_delete(delete['id'], delete['user_id']) is False:
+                return False
+        elif 'limit' in data:
+            if self.on_limit(json.loads(data)['limit']['track']) is False:
+                return False
+        elif 'warning' in data:
+            warning = json.loads(data)['warnings']
+            print warning['message']
+            return false
 
     def on_error(self, status):
         print(status)
 
-    def word_in_text(word, text):
-        word = word.lower()
-        text = text.lower()
-        match = re.search(regex, text)
-        if match:
-            return match.group()
-        return ''
+    def on_delete(self, status_id, user_id):
+        self.delout.write( str(status_id) + "\n")
+        return
+
+    def on_limit(self, track):
+        sys.stderr.write(track + "\n")
+        return
+
+    def on_error(self, status_code):
+        sys.stderr.write('Error: ' + str(status_code) + "\n")
+        return False
+
+    def on_timeout(self):
+        sys.stderr.write("Timeout, sleeping for 60 seconds...\n")
+        time.sleep(60)
+        return
 
     def tweetStatus(self, status):
         if self.count <= 10:
@@ -59,14 +89,3 @@ class Listener(tweepy.StreamListener):
         for tweet in self.tweets:
             f.write(json.dumps(tweet.__dict__))
             f.close()
-
-
-if __name__ == '__main__':
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = Listener()
-    auth = OAuthHandler(config["consumer_key"], config["consumer_secret"])
-    auth.set_access_token(config["access_token"], config["access_token_secret"])
-    stream = Stream(auth, l)
-
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['python', 'javascript', 'ruby'])

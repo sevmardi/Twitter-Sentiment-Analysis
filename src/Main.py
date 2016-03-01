06de8ae32db49584c83304a89d7897d4ac735e44
Sevak Mardirosian <smardirosians@gmail.com>
2016-03-01 21:55:09 +0100

    little changes

--- src/Main.py
+++ src/Main.py
@@ -1,16 +1,18 @@
-import json
-from src.views import View
-""" Read the config file"""
-f = open("config/config.json")
-config = json.loads(f.read())
-f.close()
+
+from src.views import MainPanel
+
+
+# """ Read the config file"""
+# f = open("config/config.json")
+# config = json.loads(f.read())
+# f.close()
 
 
 class Main(object):
-    def run(self):
-        view = View()
+    def __init__(self):
+        self.view = MainPanel.view()
+
 
 
 if __name__ == '__main__':
     app = Main()
-    app.run()
